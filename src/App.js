2a2d181e7792f8342d6516165845da700e5685cc
Alexandr <alexandr@Alexandrs-Mac-mini.local>
2022-05-29 14:59:08 +0300

    середина 4 урока

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { useState } from 'react';
 import MinMax from './les3/MinMax';
 import Modal from './les4/Modal';
+import BModal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 export default function () {
   let [products, setProducts] = useState(productStub());
   let [showDetails, setShowDetails] = useState(false);
+  let [showFaq, setShowFaq] = useState(false);
 
   let total = products.reduce((sum, pr) => sum + pr.price * pr.cnt, 0);
   // let total = useMemo(() => products.reduce((sum, pr) => sum + pr.price * pr.cnt, 0), [products]);
@@ -20,39 +23,55 @@ export default function () {
 
   return <div className='container mt-1'>
     <h1>Products List</h1>
-    <table>
-      <tbody>
-        <tr>
-          <th>#</th>
-          <th>Title</th>
-          <th>Price</th>
-          <th>Cnt</th>
-          <th>Total</th>
-          <th>Action</th>
-        </tr>
-        {products.map((pr, i) => (
-          <tr key={pr.id}>
-            <td>{i + 1}</td>
-            <td>{pr.title}</td>
-            <td>{pr.price}</td>
-            <td><MinMax min={1} max={pr.rest} current={pr.cnt} onChange={cnt => setCnt(pr.id, cnt)} /></td>
-            <td>{pr.price * pr.cnt}</td>
-            <td>
-              <button type='button' onClick={() => removeProduct(pr.id)}>x</button>
-              <button type='button' onClick={() => setCnt(pr.id, pr.rest)}>Max</button>
-            </td>
-          </tr>
-        )
-        )}
-      </tbody>
-    </table>
     <hr />
     <strong onClick={() => setShowDetails(true)}>Total: {total}</strong>
     <Modal
       showed={showDetails}
       title={`${products.length} in list, please pay order`}
-      onClose={() => setShowDetails(false)}
-    />
+      onClose={() => setShowDetails(false)}>
+      <table>
+        <tbody>
+          <tr>
+            <th>#</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Cnt</th>
+            <th>Total</th>
+            <th>Action</th>
+          </tr>
+          {products.map((pr, i) => (
+            <tr key={pr.id}>
+              <td>{i + 1}</td>
+              <td>{pr.title}</td>
+              <td>{pr.price}</td>
+              <td><MinMax min={1} max={pr.rest} current={pr.cnt} onChange={cnt => setCnt(pr.id, cnt)} /></td>
+              <td>{pr.price * pr.cnt}</td>
+              <td>
+                <button type='button' onClick={() => removeProduct(pr.id)}>x</button>
+                <button type='button' onClick={() => setCnt(pr.id, pr.rest)}>Max</button>
+              </td>
+            </tr>
+          )
+          )}
+        </tbody>
+      </table>
+    </Modal>
+    <hr />
+    <footer>
+      <strong onClick={() => setShowFaq(true)}>FAQ</strong>
+      <BModal show={showFaq} onHide={() => setShowFaq(false)}>
+        <BModal.Header closeButton>
+          Attention
+        </BModal.Header>
+        <BModal.Body>
+          <p>Hello bootstrap</p>
+        </BModal.Body>
+        <BModal.Footer>
+          <Button variant="secondary">Close</Button>
+          <Button variant="primary">Save changes</Button>
+        </BModal.Footer>
+      </BModal>
+    </footer>
   </div>;
 }
 
--- src/les4/Modal.js
+++ src/les4/Modal.js
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import useClickOutSide from './hooks/useClickOutSide';
 
-export default function ({ showed, title, onClose }) {
+export default function ({ showed, title, onClose, children }) {
   let classes = ['alert', 'alert-success'];
 
   if (!showed) {
@@ -15,9 +15,16 @@ export default function ({ showed, title, onClose }) {
       onClose();
     }
   });
+
+  let content = !children ? null : <>
+    {children}
+    <hr />
+  </>
+
   return <div ref={root} className={classes.join(' ')}>
     <h2>{title}</h2>
     <hr />
+    {content}
     <button className='btn btn-success' onClick={onClose}>Ok</button>
   </div>
 }
